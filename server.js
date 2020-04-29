776a574298d27adfc2d0850cc407814b7de80f62
Tymothy Sellers <tsellers1990@gmail.com>
2020-04-29 17:34:54 -0600

    built out the loose framework of server.js

--- server.js
+++ server.js
@@ -2,3 +2,20 @@ const express = require("express");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/hamburgerController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening at " + PORT)
+})
