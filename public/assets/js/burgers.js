b78a66406563122faff49e0a8e04248d36b19358
Tymothy Sellers <tsellers1990@gmail.com>
2020-04-30 09:42:45 -0600

    working through some bugs!

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -15,6 +15,38 @@ $(function(){
                 console.log("changed isEaten to", newEatenState);
                 location.reload();
             }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#ca").val().trim(),
+            isEaten: $("[name = isEaten]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger".on("click", function(event) {
+        var id = $(this).data("id");
+
+        $.ajax("/api/burgers" + id, {
+            type: "DELETE"
+        }).then(
+            function() {
+                console.log("deleted burger", id);
+                location.reload();
+            }
         )
-    })
-})
+    }))
+});
